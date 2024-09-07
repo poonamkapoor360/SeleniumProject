a9f350c3efa5f24f0d918c24a59de7cc1efe9715
AGL <AGL@192.168.1.4>
2024-09-08 01:44:23 +0530

    commit code

--- src/test/java/Testgithub.java
+++ src/test/java/Testgithub.java
@@ -1,4 +1,10 @@
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Testgithub {
-
+public static void main(String[] args) {
+	ChromeDriver driver = new ChromeDriver();
+	driver.get("https://www.google.com");
+	driver.manage().window().maximize();
+	
+}
 }
